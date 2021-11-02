132dc4cd8af196ad389d3c1792c9806b64e744bd
salvable <salvable@kakao.com>
2021-11-02 19:14:31 +0900

    navigation + metatial ui 구현

---
+++ RiotStats/src/component/home.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react'
+import {Button} from "@material-ui/core";
+
+
+
+const Home = () => {
+
+    return (
+        <div>
+            <Button> Button Test!!</Button>
+            <text> test</text>
+        </div>
+);
+
+};
+
+export default Home;
