ed769d15032fec97636527aac3060e1d5f825496
salvable <salvable@kakao.com>
2021-11-23 21:00:34 +0900

    검색을 했을 시 summoner의 Id 정보를 가져오는 요청 작성

--- RiotStats/src/component/profile.js
+++ RiotStats/src/component/profile.js
@@ -2,17 +2,29 @@ import React, {useEffect, useState} from 'react'
 import {Button, TextField} from "@material-ui/core";
 import {View} from "react-native";
 import {Text} from "react-native-web";
+import axios from "axios";
 
 
 
 const Profile = ({ navigation, route }) => {
+    // summoner는 accountId, id, name, profileIconId, puuid,summonerLevel을 가짐
+    const [summonerInfo,setSummonerInfo] = useState("")
+
+    useEffect(() => {
+        async function getSummoner(){
+            const summonerInfo = await axios.get(`http://localhost:3000/summonerInfo/${route.params.userId}`)
+            setSummonerInfo(summonerInfo.data.summoner)
+        }
+        getSummoner()
+    }, []);
+
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>This is {route.params.userId}'s profile</Text>
-        </View>
-    );
 
-};
+        </View>
+    )
+}
 
 export default Profile;
